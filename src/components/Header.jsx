492920941ff88adebb80f4aceb32bc427620a05f
Juan Manuel <manuz.aleman@gmail.com>
2021-07-07 15:18:43 -0500

    se agrego la funcion del player CLASE 25

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import gravatar from '../utils/gravatar';
 import '../assets/styles/components/Header.scss';
+import { logoutRequest } from '../actions';
 import logo from '../assets/static/logo-platzi-video-BW2.png';
 import userIcon from '../assets/static/user-icon.png';
 
 const Header = (props) => {
   const { user } = props;
-  const hasUser = Object.keys(user).lenght > 0;
+  const hasUser = Object.keys(user).length > 0;
+  const handleLogout = () => {
+    props.logoutRequest({});
+  };
   return (
     <header className='header'>
       <Link to='/'>
@@ -24,12 +29,14 @@ const Header = (props) => {
           <p>Perfil</p>
         </div>
         <ul>
-          <li><a href='/'>Cuenta</a></li>
-          <li>
-            <Link to='/login'>
-              Iniciar Sesión
-            </Link>
-          </li>
+          {hasUser ?
+            <li><a href='/'>{user.name}</a></li> :
+            null
+          }
+          {hasUser ?
+            <li><a href='#logout' onClick={handleLogout}>Cerrar Sesión</a></li> :
+            <li><Link to='/login'>Iniciar Sesión</Link></li>
+          }
         </ul>
       </div>
     </header>
@@ -40,4 +47,9 @@ const mapStateToProps = (state) => {
     user: state.user,
   };
 };
-export default connect(mapStateToProps, null)(Header);
+
+const mapDispatchToProps = {
+  logoutRequest,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
